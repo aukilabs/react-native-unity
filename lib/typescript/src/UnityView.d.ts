85cdbd8a8244ad6e014553f1a3ca3cdb7bfa0925
Jonathan Borg <jonathan@aukilabs.com>
2024-12-09 13:01:30 +0800

    fix: make packages using npm work

---
+++ lib/typescript/src/UnityView.d.ts
@@ -0,0 +1,25 @@
+import React from 'react';
+import type { DirectEventHandler } from 'react-native/Libraries/Types/CodegenTypes';
+type UnityViewContentUpdateEvent = Readonly<{
+    message: string;
+}>;
+type RNUnityViewProps = {
+    androidKeepPlayerMounted?: boolean;
+    fullScreen?: boolean;
+    onUnityMessage?: DirectEventHandler<UnityViewContentUpdateEvent>;
+    onPlayerUnload?: DirectEventHandler<UnityViewContentUpdateEvent>;
+    onPlayerQuit?: DirectEventHandler<UnityViewContentUpdateEvent>;
+};
+export default class UnityView extends React.Component<RNUnityViewProps> {
+    ref: React.RefObject<React.Component<import("./specs/UnityViewNativeComponent").NativeProps, {}, any> & Readonly<import("react-native").NativeMethods>>;
+    postMessage: (gameObject: string, methodName: string, message: string) => void;
+    unloadUnity: () => void;
+    pauseUnity(pause: boolean): void;
+    resumeUnity(): void;
+    windowFocusChanged(hasFocus?: boolean): void;
+    private getProps;
+    componentWillUnmount(): void;
+    render(): React.JSX.Element;
+}
+export {};
+//# sourceMappingURL=UnityView.d.ts.map
